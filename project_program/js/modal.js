64ad2fb15c844164f684a0602a2af695ff95c79f
GeekMyrZk <150453776+GeekMyrZk@users.noreply.github.com>
2024-01-02 22:35:43 +0600

    Add files via upload

---
+++ project_program/js/modal.js
@@ -0,0 +1,54 @@
+////////////////////////////////// MODAL /////////////////////////////////////
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const closeModalBtn = document.querySelector('.modal_close')
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+
+modalTrigger.onclick = () => openModal()
+closeModalBtn.onclick = () => closeModal()
+modal.onclick = (event) => event.target === modal && closeModal()
+
+setTimeout(()=> openModal(), 10000)
+const isPageBottom = () => {
+    return window.innerHeight + window.scrollY >= document.documentElement.offsetHeight
+}
+const openModalOnce = () => {
+    if (isPageBottom()) {
+        openModal()
+        window.removeEventListener('scroll', openModalOnce)
+    }
+}
+window.addEventListener('scroll', openModalOnce)
+
+//POST DATA
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const request = new XMLHttpRequest()
+    request.open("POST", "server.php")
+    request.setRequestHeader('Content-type', 'application/json')
+
+    const  formData = new FormData(form)
+    const obj = {}
+    formData.forEach((item, index) => {
+        obj[index] = item
+    })
+
+    const json = JSON.stringify(obj)
+
+    request.send(json)
+})
+
+
+
+
